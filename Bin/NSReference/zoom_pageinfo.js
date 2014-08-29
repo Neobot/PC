0263b49cb5eee62226e96bfb41a800a680f15b9f
th <trabillard@laposte.net>
2014-08-29 12:44:55 +0200

    Update ns doc

--- Bin/NSReference/zoom_pageinfo.js
+++ Bin/NSReference/zoom_pageinfo.js
@@ -3,10 +3,14 @@ pageinfo = [[0,0,0,0,null],
 	[0,0,0,0,null],
 	[0,0,0,0,null],
 	[0,0,0,0,null],
+	[0,0,0,0,null],
+	[0,0,0,0,null],
 	[0,0,0,0,null]];
-pagedata = [ ["aliases.htm","Aliases","Aliases Top Next Points Rect ",""],
+pagedata = [ ["alias.htm","Reference &#62; Alias","Alias",""],
+["ax12.htm","Reference &#62; Actuators","Actuators",""],
+["basic_actions.htm","Reference &#62; Basic Actions","Basic Actions",""],
+["basic_types.htm","Reference &#62; Basic Types","Basic Types",""],
 ["movements.htm","Reference &#62; Movements","Movements",""],
-["alias.htm","Reference &#62; Alias","Alias",""],
+["neobotscriptreference_ftsearch.htm","No title","Neobot Script Reference Contents | Index | Search Enter one or more keywords to search (&#39;*&#39; and &#39;?&#39; wildcards are supported): ",""],
 ["sensors.htm","Reference &#62; Sensors","Sensors",""],
-["syntax.htm","Reference &#62; Introduction","Introduction",""],
-["basic_types.htm","Reference &#62; Basic Types","Basic Types",""]];
+["syntax.htm","Reference &#62; Introduction","Introduction",""]];
